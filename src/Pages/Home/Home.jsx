0aaf8f2fcf69252edd04d752834cd5de9dfc5c66
raghadSammour <raghad.sammour.1999@gmail.com>
2025-08-21 15:14:13 +0200

    new update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,14 +8,6 @@ import smallSquare from "../../assets/Icons/smallSquare.png"
 import TitleText from "../../Components/TitleText/TitleText"
 import Faq from "../../Components/Faq/Faq"
 import Thanks from "../../Components/Thanks/thanks"
-
-import "./Home.css";
-import bgFAQ from "../../assets/imgs/Home/FAQ.webp";
-import bgChoose from "../../assets/imgs/Home/why choose section.webp";
-import bg from "../../assets/imgs/Home/ourservices.webp";
-import bgAbout from "../../assets/imgs/Home/What our Clients say About us.webp";
-import smallSquare from "../../assets/Icons/smallSquare.png";
-import TitleText from "../../Components/TitleText/TitleText";
 import HeroHome from "../../Components/HeroHome/HeroHome";
 import image from "../../assets/imgs/Home/hero.webp";
 import Header from "../../Components/Header/Header";
@@ -133,44 +125,36 @@ const Home = () => {
   return (
     <>
 
-  <TitleText
+  {/* <TitleText
   title="Our Services"
   text="Transform your brand with our innovative digital solutions that captivate and engage your audience."
 image={bg}
 overlay={smallSquare}
-/>
+/> */}
 
-
+{/* 
 <TitleText
   title="Why Choose SquareUp?"
   text="Experience excellence in digital craftsmanship with our team of skilled professionals dedicated to delivering exceptional results."
 image={bgChoose}
 overlay={smallSquare}
-/>
+/> */}
 
-<TitleText
+{/* <TitleText
   title="What our Clients say About us"
   text="At SquareUp, we take pride in delivering exceptional digital products and services that drive success for our clients. Here's what some of our satisfied clients have to say about their experience working with us"
 image={bgAbout}
 overlay={smallSquare}
-/>
+/> */}
 
-
+{/* 
 <TitleText
   title="Frequently Asked Questions"
   text="Still you have any questions? Contact our Team via hello@squareup.com"
 image={bgFAQ}
 overlay={smallSquare}
-/>
-<Faq />
-<Thanks  title="Thank you for your Interest in SquareUp." Text="We would love to hear from you and discuss how we can help bring your digital ideas to life. Here are the different ways you can get in touch with us."/>
-</>
-  )
-}
-
-export default Home
-
-      <Header className="RB_Header">
+/> */}
+ {/* <Header className="RB_Header">
         <HeroHome
           image={image}
           title="A Digital Product Studio
@@ -185,7 +169,7 @@ export default Home
           image={bg}
           overlay={smallSquare}
         />
-      </Header>
+      </Header> */}
 
 
       <HeroHome
@@ -200,12 +184,7 @@ export default Home
           <Companies key={index} name={company.name} logo={company.logo} />
         ))}
       </ul>
-      <TitleText
-        title="Our Services"
-        text="Transform your brand with our innovative digital solutions that captivate and engage your audience."
-        image={bg}
-        overlay={smallSquare}
-      />
+
       <TitleText
         title="Our Services"
         text="Transform your brand with our innovative digital solutions that captivate and engage your audience."
@@ -266,9 +245,14 @@ export default Home
         image={bgFAQ}
         overlay={smallSquare}
       />
-    </>
-  );
-};
+      <Faq />
+<Thanks  title="Thank you for your Interest in SquareUp." Text="We would love to hear from you and discuss how we can help bring your digital ideas to life. Here are the different ways you can get in touch with us."/>
 
-export default Home;
+</>
+  )
+}
+
+export default Home
+
+     
 
